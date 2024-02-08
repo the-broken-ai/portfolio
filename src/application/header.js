2f77afd8ee9a6a5f4167a4e2c1e0053447f5aae0
NotSujanSharma <ssharma627@myseneca.ca>
2024-02-07 23:48:14 -0500

    added animation when hovered in profile and message icon

--- src/application/header.js
+++ src/application/header.js
@@ -5,7 +5,7 @@ class Header extends React.Component {
     return (
       <div className="h-20 flex flex-row bg-[#0d1116] text-white items-center justify-between border-b-[1px] border-[#26507f]">
         <div className="flex flex-row">
-          <div className="h-12 w-12 bg-[#0d1116] border-2 rounded-full  border-[#334458] mx-10 overflow-hidden cursor-pointer">
+          <div className="h-12 w-12 bg-[#0d1116] border-2 rounded-full  border-[#334458] mx-10 overflow-hidden cursor-pointer  hover:translate-y-[-2px] transition-transform">
             <img
               src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png"
               alt=""
@@ -15,12 +15,11 @@ class Header extends React.Component {
             <input
               type="text"
               className="bg-[#0d1116]  w-[50vw] md:w-[20em] hover:w-auto md:hover:w-[60vw]  h-12 rounded-md border-[#334458]  border-2 px-3 text-white transition-all duration-300"
-              style={{ fontSize: "20px" }}
-              placeholder="Search"
+              placeholder="Type @ to filter by people"
             />
           </div>
         </div>
-        <div className="h-10 w-10 mx-10 cursor-pointer">
+        <div className="h-10 w-10 mx-10 cursor-pointer hover:translate-y-[-2px] transition-transform">
           <img
             src="https://img.icons8.com/ios-filled/50/FFFFFF/speech-bubble--v1.png"
             alt=""

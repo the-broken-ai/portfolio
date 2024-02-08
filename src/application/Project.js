361f4f0d75d92a2c41090e8ad11c73ccad4108bd
NotSujanSharma <ssharma627@myseneca.ca>
2024-02-07 23:58:43 -0500

    added project component

---
+++ src/application/Project.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Project() {
+  return <div className="text-white">Project Goes Here</div>;
+}
+
+export default Project;
--- src/application/body.js
+++ src/application/body.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Project from "./Project.js";
 
 const Body = () => {
   return (
     <div className="bg-[#04080d] text-white h-full w-full px-10 py-4">
       <h1 className="text-3xl font-bold">
-        Me is body
+        <Project />
       </h1>
     </div>
   );

a3ede3f48b4a56bc7f667042d89f5f58518f3a04
notsujansharma <sujan@brokenai.ca>
2024-02-11 01:30:12 -0500

    fetching data from different server

--- src/application/Project.js
+++ src/application/Project.js
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Project({ post }) {
+  const [user, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch(post.author, {
+      headers: {
+        Authorization: "Token 3645eaa1b08c5a27041a2f04872bc8efe207a1f4",
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => setPosts(data))
+      .catch((error) => console.log(error));
+  }, [post.author]);
+
   return (
     <div className="text-white w-full py-3 px-[5vw] md:px-[20vw] ">
       <div className="border-b-[1px] border-[#393a47]">
         <div className="font-bold mt-4 flex flex-row items-baseline">
           <div className="text-2xl">
-            <a href={post.github}>{post.title}</a>
+            <a href={post.repository}>{post.title}</a>
           </div>
+
           <div className="ml-3">
-            {/* it is not logging into console */}
-            <a href={`/api/users/${post.username}`}>{post.username}</a>
-            {console.log(post.github)}
+            <a href={post.author}>{user.username}</a>
           </div>
+          {console.log(user.username)}
         </div>
         <div className="text-l font-bold my-2">{post.content}</div>
       </div>
--- src/application/body.js
+++ src/application/body.js
@@ -5,7 +5,12 @@ const Body = () => {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    fetch("/api/getdata")
+    fetch("/posts/?format=json", {
+      headers: {
+        Authorization: "Token 3645eaa1b08c5a27041a2f04872bc8efe207a1f4",
+        "Content-Type": "application/json",
+      },
+    })
       .then((response) => response.json())
       .then((data) => setPosts(data))
       .catch((error) => console.log(error));
@@ -13,9 +18,8 @@ const Body = () => {
 
   return (
     <div className="bg-[#04080d] text-white h-full w-full  py-4">
-      {posts.map((post) => (
-        <Project post={post} />
-      ))}
+      {posts.results && posts.results.map((post) => <Project post={post} />)}
+      {/* {console.log(posts)} */}
     </div>
   );
 };
